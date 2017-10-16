c04f8f630d13e3eb29bcad2a35564b9cd65fbf43
rolextereso <rolexjun098@gmail.com>
2017-10-17 02:16:46 +0800

    Created Merchant Design(Brain) folder for brain webpage

--- Merchant Design (Brain)/js/myscript.js
+++ Merchant Design (Brain)/js/myscript.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
         			$(window).scroll(function() {
         				
-					  if ($(document).scrollTop() > 400) {
+					  if ($(document).scrollTop() > 425) {
 					    $('#promo').addClass('shrink');
 					  } else {
 					    $('#promo').removeClass('shrink');
